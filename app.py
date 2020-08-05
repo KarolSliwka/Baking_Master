9e9df1145779152fe8fc5b6979c3f3a30126d802
EC2 Default User <ec2-user@ip-172-31-19-177.eu-west-2.compute.internal>
2020-08-05 19:30:52 +0000

    Body ID added to all routes

--- app.py
+++ app.py
@@ -9,47 +9,47 @@ app = Flask(__name__)
 @app.route('/')
 @app.route('/home')
 def home():
-    return render_template('layout/base.html')
+    return render_template('layout/base.html', body_id='home-page')
 
 @app.route('/recipes')
 def recipes():
-    return  render_template('pages/recipes.html')
+    return  render_template('pages/recipes.html', body_id='recipes-page')
     
 @app.route('/favouries')
 def favourites():
-    return render_template('pages/favourites.html')
+    return render_template('pages/favourites.html', body_id='favourites-page')
     
 @app.route('/search_results')
 def search_results():
-    return render_template('pages/search-results.html')
+    return render_template('pages/search-results.html', body_id='search-page')
 
 @app.route('/contact')
 def contact():
-    return render_template('pages/contact.html')
+    return render_template('pages/contact.html', body_id='contact-page')
     
 @app.route('/login')
 def login():
-    return render_template('pages/login.html', body_id="login-page")
+    return render_template('pages/login.html', body_id='login-page')
     
 @app.route('/register')
 def register():
-    return render_template('pages/register.html')
+    return render_template('pages/register.html', body_id='register-page')
     
 @app.route('/pass-recovery')
 def recovery():
-    return render_template('pages/pass-recovery.html')
+    return render_template('pages/pass-recovery.html', body_id='recovery-page')
 
 @app.route('/about')
 def about():
-    return  render_template('pages/about.html')
+    return  render_template('pages/about.html', body_id='about-page')
     
 @app.route('/site-map')
 def site_map():
-    return  render_template('pages/site-map.html')
+    return  render_template('pages/site-map.html', body_id='site-map-page')
 
 @app.route('/equipment')
 def equipment():
-    return  render_template('pages/equipment.html')
+    return  render_template('pages/equipment.html', body_id='equipment-page')
 
 if __name__ == '__main__':
     app.run(host=os.environ.get('IP'),
