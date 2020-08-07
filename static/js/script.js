36c4ebea7b223f38812fec460148e160233043b5
EC2 Default User <ec2-user@ip-172-31-19-177.eu-west-2.compute.internal>
2020-08-07 17:38:55 +0000

    Contact form clear form script added

--- static/js/script.js
+++ static/js/script.js
@@ -2,5 +2,15 @@
 
 /**
  * This functions is taking current full year and passing it into copyrights section
-*/
+ */
 $('#copyright').html(new Date().getFullYear());
+
+
+/**
+ * This function will clear contact form
+ */
+$('#clear-contact-form').click(function() {
+    $('#contact-name').val('');
+    $('#contact-email').val('');
+    $('#contact-message').val('');
+});
